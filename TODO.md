1b950e0452d3e2522e3afe3a990d30313fab9dd2
Anthony <developedbyant@gmail.com>
2022-12-09 15:36:18 -0500

    NEW delete route's linked data when route is deleted

--- TODO.md
+++ TODO.md
@@ -1,3 +1,4 @@
 ### Things that need to get done
 - [ ] Protect admin api
 - [ ] Allow multiple asset elements
+- [ ] make sure routeID do not start with __ , we use it for important keys
--- src/routes/admin/api/routes/[routeID]/route-delete/+server.ts
+++ src/routes/admin/api/routes/[routeID]/route-delete/+server.ts
@@ -1,9 +1,9 @@
 import db from "$Database"
 import svelteCMS from "$svelteCMS"
 import { json } from "@sveltejs/kit"
-// @ts-ignore
-import type { RequestHandler } from "./$Types"
+import type { RequestHandler } from "./$types"
 import type { DeleteRouteLoad,DeleteRouteRes } from "$Types/api"
+import type { RouteData } from "$Types"
 
 export const POST:RequestHandler = async({params,request}) => {
     const {routeID} = params
@@ -13,6 +13,7 @@ export const POST:RequestHandler = async({params,request}) => {
     const routeCollectionsArray =  await db.listCollections().toArray()
     /** Check route id exists */
     const routeIDExists = await routesCollection.findOne({ ID:routeID })
+    // If route do not exists, return error
     if(!routeIDExists){
         const response:DeleteRouteRes = { ok:false,msg:`Route:${routeID} do not exists`}
         return json(response)
@@ -32,7 +33,26 @@ export const POST:RequestHandler = async({params,request}) => {
     if(routeCollectionsArray.find(data=>data.name===`${svelteCMS.config.tcb}_${routeID}`)){
         db.collection(`${svelteCMS.config.tcb}_${routeID}`).drop()
     }
+    // Handle route deleted
+    handleRouteDeleted(jsonData)
     // Return response
     const response:DeleteRouteRes = { ok:true,msg:`Route:${routeID} was delete` }
     return json(response) 
+}
+
+/** Delete any linked assets,tags and categories to this route */
+async function handleRouteDeleted(route:RouteData){
+    // Drop all linked assets to this route
+    const linkedAssetsCollection = db.collection(svelteCMS.collections.linkedAssets)
+    linkedAssetsCollection.deleteMany({ collection:route.ID })
+    // Drop all linked tags to this route
+    if(route.includeTags==="yes"){
+        const linkedTagsCollection = db.collection(svelteCMS.collections.linkedTags)
+        linkedTagsCollection.deleteMany({ collection:route.ID })
+    }
+    // Drop all linked categories to this route
+    if(route.includeTags==="yes"){
+        const linkedCategoriesCollection = db.collection(svelteCMS.collections.linkedCategories)
+        linkedCategoriesCollection.deleteMany({ collection:route.ID })
+    }
 }
